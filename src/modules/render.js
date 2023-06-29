8fa23c5a35630f5073822cf4b0fc0ca4c3ebd25a
DevaWinner <winneraaniekan@gmail.com>
2023-06-29 21:19:07 +0100

    Correct linter errors

--- src/modules/render.js
+++ src/modules/render.js
@@ -1,28 +1,27 @@
-
 const leaderboardData = [
-  { 
-    name: 'Player 1', 
-    score: 100 
+  {
+    name: 'Player 1',
+    score: 100,
   },
-  { 
-    name: 'Player 2', 
-    score: 200 
+  {
+    name: 'Player 2',
+    score: 200,
   },
-  { 
-    name: 'Player 3', 
-    score: 150 
+  {
+    name: 'Player 3',
+    score: 150,
   },
   {
     name: 'Player 4',
-    score: 300
+    score: 300,
   },
   {
     name: 'Player 5',
-    score: 250
+    score: 250,
   },
   {
     name: 'Player 6',
-    score: 350
+    score: 350,
   },
 ];
 
