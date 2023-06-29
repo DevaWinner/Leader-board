59e5e29533a496bc369bf4e6fcbd2889794e38af
DevaWinner <winneraaniekan@gmail.com>
2023-06-29 21:15:40 +0100

    Import and render leaderboard

--- src/index.js
+++ src/index.js
@@ -1,33 +1,4 @@
 import './styles/style.css';
-
-
-const leaderboardData = [
-  { 
-    name: 'Player 1', 
-    score: 100 
-  },
-  { 
-    name: 'Player 2', 
-    score: 200 
-  },
-  { 
-    name: 'Player 3', 
-    score: 150 
-  },
-];
-
-const leaderboard = document.getElementById('leaderboard');
-
-const renderLeaderboard = () => {
-  leaderboard.innerHTML = '';
-  leaderboardData.forEach((player) => {
-    const li = document.createElement('li');
-    li.innerHTML = `
-      <span>${player.name}:</span>
-      <span>${player.score}</span>
-  `;
-    leaderboard.appendChild(li);
-  });
-};
+import renderLeaderboard from './modules/render.js';
 
 renderLeaderboard();
