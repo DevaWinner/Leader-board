95f930deb715c98402af24665ef06b48a699ecde
DevaWinner <winneraaniekan@gmail.com>
2023-06-29 21:09:53 +0100

    Create dynamic board data

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 import './styles/style.css';
 
 
+const leaderboardData = [
+  { 
+    name: 'Player 1', 
+    score: 100 
+  },
+  { 
+    name: 'Player 2', 
+    score: 200 
+  },
+  { 
+    name: 'Player 3', 
+    score: 150 
+  },
+];
+
