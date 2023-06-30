56381805d39bef11c6b8c3276f558948523690c0
DevaWinner <winneraaniekan@gmail.com>
2023-06-30 12:44:57 +0100

    Add function to render content

--- src/modules/render.js
+++ src/modules/render.js
@@ -1,1 +1,16 @@
 import { getData } from './game.js';
+
+const render = async () => {
+  const data = await getData();
+  const recentScore = document.getElementById('leaderboard');
+  recentScore.innerHTML = '';
+  const sortedData = data.sort((a, b) => a.score - b.score);
+  sortedData.forEach((item) => {
+    const listItem = document.createElement('li');
+    listItem.innerHTML = `
+    <span> ${item.user}: </span>
+    <span> ${item.score} </span>
+    `;
+    recentScore.appendChild(listItem);
+  });
+};
