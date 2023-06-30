c0f560f43aa484fdac54ade8d721c3e56567f2ea
DevaWinner <winneraaniekan@gmail.com>
2023-06-30 12:54:41 +0100

    Clean linter errors on index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './styles/style.css';
 import submitForm from './modules/submit.js';
-import {render, renderLeaderboardOnLoad} from './modules/render.js';
+import { render, renderLeaderboardOnLoad } from './modules/render.js';
 
 const refreshBtn = document.getElementById('refresh-btn');
 const submitBtn = document.getElementById('submit-btn');
